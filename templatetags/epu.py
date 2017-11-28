6b8c794be642a53b8c39dca918e507f1a0a6bc93
Steven Dwy <yoplitein+git@gmail.com>
2017-11-27 19:00:41 -0800

    Fix set template tag

--- templatetags/epu.py
+++ templatetags/epu.py
@@ -7,9 +7,9 @@ def set(context, *values, **kwargs):
     name = next(node for node in context["block"].nodelist if node.__class__.__name__ == "SimpleNode").args[0].var.var
     
     if kwargs.get("default", False):
-        name = context.get(name, None)
+        value = context.get(name, None)
         
-        if name != None:
+        if value != None:
             return ""
     
     value = kwargs.get("sep", " ").join(values[1:])
