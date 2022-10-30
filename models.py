f08d9355f190ccc2779d3aab3dca58d3deb260ca
Steven Dwy <yoplitein+git@gmail.com>
2022-10-30 16:24:47 -0700

    Better display names/descriptions for model fields

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class User(models.Model):
     base = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
-    apiKey = models.CharField(max_length=64, blank=True)
+    apiKey = models.CharField(max_length=64, blank=True, verbose_name="API key", help_text="Leave blank to regenerate.")
     
     def save(self, *args, **kwargs):
         import hashlib
@@ -28,14 +28,14 @@ class AuthKeys(models.Model):
 class UpdaterBins(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    description = models.CharField(max_length=160)
-    winZip = models.FileField(upload_to="epu/zips/")
-    macZip = models.FileField(upload_to="epu/zips/")
-    linuxZip = models.FileField(upload_to="epu/zips/")
+    description = models.CharField(max_length=160, help_text="Use to record e.g. the git tag+hash the zips were built from.")
+    winZip = models.FileField(upload_to="epu/zips/", verbose_name="Windows zip")
+    macZip = models.FileField(upload_to="epu/zips/", verbose_name="macOS zip")
+    linuxZip = models.FileField(upload_to="epu/zips/", verbose_name="Linux zip")
     
     class Meta:
-        verbose_name = "Updater Binary"
-        verbose_name_plural = "Updater Binaries"
+        verbose_name = "Client Binary"
+        verbose_name_plural = "Client Binaries"
     
     def __str__(self):
         return "Binaries created {} ({})".format(self.created.strftime("%Y-%m-%d/%H:%M"), self.description)
@@ -65,11 +65,11 @@ class UpdaterBins(models.Model):
         super().delete(*args, **kwargs)
 
 class Pack(models.Model):
-    name = models.CharField(max_length=1024)
-    slug = models.CharField(max_length=256)
-    gitURL = models.CharField(max_length=1024)
-    instanceZip = models.FileField(upload_to="epu/zips/")
-    updaterBinaries = models.ForeignKey(UpdaterBins, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=1024, verbose_name="Display name")
+    slug = models.CharField(max_length=256, verbose_name="URL-friendly name")
+    gitURL = models.CharField(max_length=1024, verbose_name="Git repository")
+    instanceZip = models.FileField(upload_to="epu/zips/", verbose_name="Instance zip")
+    updaterBinaries = models.ForeignKey(UpdaterBins, null=True, on_delete=models.SET_NULL, verbose_name="Client binaries")
     icon = models.FilePathField(path=os.path.join(settings.STATIC_ROOT, "epu/icons"), recursive=False, default="infinity.png")
     
     def __str__(self):
